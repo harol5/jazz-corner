4b8c48540fae45c15dd2874f4520938b4705e798
harol5 <hrcode95@gmail.com>
2024-06-27 00:32:40 -0400

    errors with cors - testing

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,6 +3,8 @@ const whiteList = [
   "http://localhost:4500",
   "http://localhost:3000",
   "https://my-simple-notes-v1-2fcafdc02472.herokuapp.com",
+  "https://jazzc.herokuapp.com:3000",
+  "https://jazzc.herokuapp.com",
 ];
 const corsOptions = {
   origin: (origin, callback) => {
